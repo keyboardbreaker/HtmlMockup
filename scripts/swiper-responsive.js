24927758ffd83d1eb7e72f8d70bdabccede18bd0
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-02-27 19:55:19 +0000

    feedback2: adding another breakpoint to swiper gallery so it includes 2 in the gallery on smaller screens instead of jumping to 1

--- scripts/swiper-responsive.js
+++ scripts/swiper-responsive.js
@@ -7,10 +7,13 @@ var swiper = new Swiper('.swiper-container', {
     },
     resizeReInit: true,
 });
+
+// 320 to 1100
 $(window).resize(function(){
     var ww = $(window).width();
-    if (ww>1400) swiper.params.slidesPerView = 3;
-    if (ww<=1400) swiper.params.slidesPerView = 1;
+    if (ww>1100) swiper.params.slidesPerView = 3;
+    if (ww>700 && ww <1100) swiper.params.slidesPerView = 2;
+    if (ww<=768) swiper.params.slidesPerView = 1;
     swiper.update();
 });
 $(window).trigger('resize');
